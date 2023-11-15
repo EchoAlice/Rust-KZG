b47888ec38c434a171339b6cb1227066ec8a92f0
EchoAlice <echoalice0@gmail.com>
2023-11-15 10:08:03 -0700

    Simplify Field trait and FieldElement struct

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 This repository is a place where I can document KZG-based concepts used to scale Ethereum.
 
-I'm creating this cryptographic library to gain a deeper understanding of 
-elliptic curve cryptography and Rust.
+The **overarching goal** of this repository is to create one of cryptography's key cryptographic primitives: the elliptic curve pairing.  
 
-Ultimately, I'd like to expose all public-facing functions that're needed to implement full-scale Danksharding.
+Pairings introduce a form of "encrypted mulitiplication", allowing us to check quadratic constraints.
 
+
+### -----------------------------------------------------------------------
 TODO: 
 - Create `blob_to_kzg_commitment` for a 2 dimensional blob.
 - Measure performance benchmarks with [Divan](https://nikolaivazquez.com/blog/divan/)
--- src/fields.rs
+++ src/fields.rs
@@ -1,57 +1,127 @@
-use std::error::Error;
-
 /*
 High Level Repository Notes:
 -------------------------------------------------
 TODO:
-    1. Create a prime field that implements this field trait.
+    1. Create a prime field that implements the field trait.
     2. Create THE field over which our curve is defined.
     3. Create the BLS12-381 G1 point type.
         - Express all functionality the point type needs.
 -------------------------------------------------
  */
 
-// A generalized version of "what a number is"
 pub trait Field {
-    fn add(&self, other: &PrimeFieldElement) -> PrimeFieldElement;
-    fn sub(&self, other: &PrimeFieldElement) -> PrimeFieldElement;
-    fn mul(&self, other: &PrimeFieldElement) -> PrimeFieldElement;
-    fn div(&self, other: &PrimeFieldElement) -> PrimeFieldElement;
+    fn add(&self, other: &Self) -> Self;
+    fn sub(&self, other: &Self) -> Self;
+    fn mul(&self, other: &Self) -> Self;
+    fn div(&self, other: &Self) -> Self;
 }
 
-// Create field elements via a finite_field.from()
-// Note: This is a Finite Field Element!
-//
-// Should i call this a "prime field element"?
+// A field element can be thought of as a generalized version of "what a number is".
+#[derive(Debug, PartialEq)]
+pub struct FieldElement {
+    element: u32,
+    modulus: u32,
+}
+
+impl FieldElement {
+    fn from(element: u32, modulus: u32) -> FieldElement {
+        Self {
+            element: element % modulus,
+            modulus,
+        }
+    }
+}
+
+impl Field for FieldElement {
+    fn add(&self, b: &Self) -> Self {
+        assert_eq!(&self.modulus, &b.modulus);
+
+        let res = (&self.element + b.element) % &self.modulus;
+        Self {
+            element: res,
+            modulus: self.modulus,
+        }
+    }
+
+    fn sub(&self, b: &Self) -> Self {
+        assert_eq!(&self.modulus, &b.modulus);
+
+        let a = i64::from(self.element);
+        let b = i64::from(b.element.clone());
+        let mut res = (a - b) % self.modulus as i64;
+
+        if res < 0 {
+            res += self.modulus as i64;
+        }
+
+        Self {
+            element: (res as u32),
+            modulus: self.modulus,
+        }
+    }
+
+    fn mul(&self, b: &Self) -> Self {
+        assert_eq!(&self.modulus, &b.modulus);
+
+        let res = (&self.element * b.element) % &self.modulus;
+
+        Self {
+            element: res,
+            modulus: self.modulus,
+        }
+    }
+
+    fn div(&self, b: &Self) -> Self {
+        assert_eq!(&self.modulus, &b.modulus);
+        assert_ne!(b.element, 0 as u32);
+
+        // Inverse of &self.element leveraging Fermat's little theorum:
+        // TODO: *Why* does Fermat's little theorum work?
+        let res = (self.element * (b.element.pow(self.modulus - 2))) % self.modulus;
+
+        Self {
+            element: res,
+            modulus: self.modulus,
+        }
+    }
+}
+
+// TODO:
+/*
+pub trait PrimeField: Field {
+    fn from() {
+        is_prime();
+    }
+    fn modulus() {}
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct PrimeFieldElement {
     int: u32,
-    field: PrimeField,
+    modulus: u32,
 }
 
-// Big Question:
-// What properties does our prime field need to have?
+// TODO:
+impl PrimeField for PrimeFieldElement {}
+ */
 
-// Note: A prime field is a type of finite field.
-// We need a prime field for our elliptic curve equation.
-//
-// TODO: Our modulus MUST be a prime number.
+// ------------------------------------------------------------------------
+/*
+
+//       TODO: Our modulus MUST be a prime number.
 #[derive(Debug, PartialEq, Clone)]
 struct PrimeField {
+    // TODO: Create a prime number type.
     modulus: u32,
-    generator: u32,
     // TODO: Add a "zero?"
 }
 
 impl PrimeField {
-    fn new(modulus: u32, generator: u32) -> Self {
-        Self { modulus, generator }
+    fn new(modulus: u32) -> Self {
+        Self { modulus }
     }
 
     fn from(&self, int: u32) -> Result<PrimeFieldElement, Box<dyn Error>> {
-        // Verifies the *integer* is within the field.
-        // Type conversion needed for division
-        let out = f64::from(int) / f64::from(self.generator);
         assert_eq!(out, out.round());
 
         Ok(PrimeFieldElement {
@@ -60,116 +130,59 @@ impl PrimeField {
         })
     }
 }
-
-impl Field for PrimeFieldElement {
-    fn add(&self, b: &Self) -> Self {
-        assert_eq!(&self.field, &b.field);
-
-        let res = (&self.int + b.int) % &self.field.modulus;
-        Self {
-            int: res,
-            field: self.field.clone(),
-        }
-    }
-
-    fn sub(&self, b: &Self) -> Self {
-        assert_eq!(&self.field, &b.field);
-
-        // Type conversion needed for subtraction
-        let a = i64::from(self.int.clone());
-        let b = i64::from(b.int.clone());
-        let mut res = (a - b) % self.field.modulus as i64;
-
-        if res < 0 {
-            res += self.field.modulus as i64;
-        }
-
-        Self {
-            int: (res as u32),
-            field: self.field.clone(),
-        }
-    }
-
-    fn mul(&self, b: &Self) -> Self {
-        assert_eq!(&self.field, &b.field);
-        let res = (&self.int * b.int) % &self.field.modulus;
-
-        Self {
-            int: res,
-            field: self.field.clone(),
-        }
-    }
-
-    fn div(&self, b: &Self) -> Self {
-        assert_ne!(b.int, 0 as u32);
-
-        // Inverse of &self.int leveraging Fermat's little theorum:
-        let res = (self.int * (b.int.pow(self.field.modulus - 2))) % self.field.modulus;
-
-        Self {
-            int: res,
-            field: self.field.clone(),
-        }
-    }
-}
+ */
+// ------------------------------------------------------------------------
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // Prime field modulo arithmetic tests
+    #[test]
+    fn modulo() {
+        let modulus = 17;
+        let res = FieldElement::from(26, modulus);
+        assert_eq!(res, FieldElement::from(9, modulus))
+    }
+
+    // Field modulo arithmetic tests
     // ------------------------------------------------------
     #[test]
-    fn add_elements() {
-        let fq = PrimeField::new(17, 1);
-        let a = fq.from(9).expect("hard coded values in test are valid");
-        let b = fq.from(10).expect("hard coded values in test are valid");
+    fn add() {
+        let modulus = 17;
+        let a = FieldElement::from(9, modulus);
+        let b = FieldElement::from(10, modulus);
 
         let res = a.add(&b);
-
-        assert_eq!(
-            res,
-            fq.from(2).expect("hard coded values in test are valid")
-        );
+        assert_eq!(res, FieldElement::from(2, modulus));
     }
 
     #[test]
-    fn sub_elements() {
-        let fq = PrimeField::new(17, 1);
-        let a = fq.from(9).expect("hard coded values in test are valid");
-        let b = fq.from(10).expect("hard coded values in test are valid");
+    fn sub() {
+        let modulus = 17;
+        let a = FieldElement::from(9, modulus);
+        let b = FieldElement::from(10, modulus);
+
         let res = a.sub(&b);
-
-        assert_eq!(
-            res,
-            fq.from(16).expect("hard coded values in test are valid")
-        );
+        assert_eq!(res, FieldElement::from(16, modulus));
     }
 
     #[test]
-    fn mul_elements() {
-        let fq = PrimeField::new(17, 1);
-        let a = fq.from(9).expect("hard coded values in test are valid");
-        let b = fq.from(10).expect("hard coded values in test are valid");
+    fn mul() {
+        let modulus = 17;
+        let a = FieldElement::from(9, modulus);
+        let b = FieldElement::from(10, modulus);
 
-        assert_eq!(
-            a.mul(&b),
-            fq.from(5).expect("hard coded values in test are valid")
-        );
+        let res = a.mul(&b);
+        assert_eq!(res, FieldElement::from(5, modulus));
     }
 
     #[test]
-    fn div_elements() {
-        let fq = PrimeField::new(7, 1);
-        let a = fq.from(3).expect("hard coded values in test are valid");
-        let b = fq.from(2).expect("hard coded values in test are valid");
+    fn div() {
+        let modulus = 7;
+        let a = FieldElement::from(3, modulus);
+        let b = FieldElement::from(2, modulus);
 
-        assert_eq!(
-            a.div(&b),
-            fq.from(5).expect("hard coded values in test are valid")
-        );
+        let res = a.div(&b);
+        assert_eq!(res, FieldElement::from(5, modulus));
     }
-
-    // Elliptic Curve Point Tests
-    // ------------------------------------------------------
 }
